6b9842c0c02a1a16cfc7ad468308bd457e973489
Nathan <nuchuang94@gmail.com>
2021-11-25 12:05:14 -0800

    Changed option for grid size so that it accepts sizes up to 100x100, centered items on page

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+function getValue() { //accept numbers only up to 100
+    let val = document.querySelector('#selection').value;
+    if (val <= 100) {
+        createGrid(val);
+    } else {
+        let tooBig = document.querySelector('#error');
+        tooBig.textContent = "VALUE IS TOO LARGE";
+    }
+}
+
 function createGrid (num) {
     let container = document.querySelector('#gridContainer');
     container.innerHTML = ''; //removes any previously placed grids
@@ -6,11 +16,10 @@ function createGrid (num) {
         let div = document.createElement('div');
         container.appendChild(div);
         div.setAttribute('id', 'cells');
+        div.onmouseover = function() { //changes the color to grey when hovering over a cell
+            div.style.backgroundColor = "grey";
+            }
         container.style.width = ((num * 24) + ((num - 1) * 2)) + "px"; //makes the container the right size for fitting it num x num
         }
 }
 
-function mouseOver () {
-    let hoverCol = document.getElementById('#columns')
-    hoverCol.style.backgroundColor = "grey";
-}
